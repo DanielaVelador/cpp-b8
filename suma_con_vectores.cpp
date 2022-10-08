d8e2d673dca8e3a67a9235ed866c5a4abf107295
Daniela Velador <54413126+DanielaVelador@users.noreply.github.com>
2022-10-08 10:15:40 -0500

    Create suma_con_vectores.cpp

---
+++ suma_con_vectores.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	float vector1[3];
+	float vector2[3];
+	float vectorSuma[3];
+	int i;
+	//Pedimos los datos del primer vector
+	for (i = 0; i < 3; i++)
+	{
+		cout << "Introduce el valor: " << i << " del primer vector:  ";
+		cin >> vector1[i];
+	}
+	//Pedimos los datos del segundo vector
+	for (i = 0; i < 3; i++)
+	{
+		cout << "Introduce el valor: " << i << " del segundo vector:  ";
+		cin >> vector2[i];
+	}
+	//Calculamos la suma
+	for (i = 0; i < 3; i++)
+	{
+		vectorSuma[i] = vector1[i] + vector2[i];
+	}
+	//Mostramos el resultado
+	cout << "El vector suma es: ";
+	for (i = 0; i < 3; i++)
+	{
+		cout << vectorSuma[i] << "   ";
+	}
+}
